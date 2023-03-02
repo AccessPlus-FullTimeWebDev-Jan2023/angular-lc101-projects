7cda39e00bbebe7a546f7b49ed256a13dea46aab
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-01 23:46:05 -0600

    buttons finished

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   inactive: boolean[] = [false, false, false, false]
+   message: string = 'Frustration Incarnate!'
+   locations: string[] = ['center', 'left', 'right']
+   location: string = 'center'
 
    constructor() { }
 
    ngOnInit() { }
+
+   resetButtons(): void {
+      for (let i=0; i<this.inactive.length; i++) {
+         this.inactive[i] = false
+      }
+   }
+
+   moveButton(oldLocation: string): void {
+      while (this.location === oldLocation) {
+         this.location = this.locations[Math.floor(Math.random()*this.locations.length)]
+      }
+   }
 
 }
