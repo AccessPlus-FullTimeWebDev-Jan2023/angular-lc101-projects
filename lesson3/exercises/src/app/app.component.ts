a09868a2774884c01479e1fac424521f0f3636d8
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-02 13:38:57 -0600

    almost

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -14,13 +14,36 @@ export class AppComponent {
   message = 'Space shuttle ready for takeoff!';
 
   handleTakeOff() {
-    let result = 
-    window.confirm('Are you sure the shuttle is ready?')
+    let result =
+      window.confirm('Are you sure the shuttle is ready?')
     if (result) {
       this.color = 'blue'
       this.height = 10000
       this.width = 0
       this.message = 'Shuttle in flight.'
+    }
+  }
+
+  handleLanding() {
+    let result = window.confirm('Confirm landing?')
+    if (result) {
+      window.alert('Shuttle is landing.')
+      this.color = 'green'
+      this.height = 0
+      this.width = 0
+      this.message = 'Space shuttle ready for takeoff!'
+    }
+  }
+
+  handleAbort() {
+    let result = window.confirm('Are you sure you want to abort?')
+    if (result) {
+      this.color = 'red'
+      window.alert('Mission aborted, return to base.')
+      this.color = 'green'
+      this.height = 0
+      this.width = 0
+      this.message = 'Space shuttle ready for takeoff!'
     }
   }
 }
